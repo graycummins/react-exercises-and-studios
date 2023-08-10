c50c81669cf6a6735bce760113e92f88d96049c5
graycummins <>
2023-08-10 11:44:53 -0500

    Changed choreList js

--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,20 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+
+import React from 'react';
+import classes from './ChoresList.module.css';
+
+class ChoreList extends React.Component {
+   render() {
+      const movies = ["Pride and Prejudice", "Howl's Moving Castle"];
+      return (
+      <div>
+         <h3 className = {classes.choresHeading} >Movies I Watched This Month</h3>
+         <ol>
+            <li className={classes.choresText}>{movies[0]}</li>
+            <li className={classes.choresText}>{movies[1]}</li>
+         </ol>
+      </div>
+      );
+   }
 }
+
+export default ChoreList;
